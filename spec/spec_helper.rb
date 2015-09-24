9987eed16b42c0a29902735cdba2a2ff0f374f46
Joao Duarte <jsvduarte@gmail.com>
2015-09-24 15:57:04 +0100

    implement close/stop and fix spec timing issue

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,10 @@
 require "logstash/devutils/rspec/spec_helper"
 require 'logstash/inputs/udp'
 
+class LogStash::Inputs::Udp
+  attr_reader :udp
+end
+
 module UdpHelpers
 
   def input(plugin, size, &block)
@@ -9,12 +13,15 @@ module UdpHelpers
     input_thread = Thread.new do
       plugin.run(queue)
     end
+    # because the udp socket is created and bound during #run
+    # we must ensure that it is open before sending data
+    sleep 0.1 until (plugin.udp && !plugin.udp.closed?)
     block.call
     sleep 0.1 while queue.size != size
     result = nevents.times.inject([]) do |acc|
       acc << queue.pop
     end
-    plugin.stop
+    plugin.do_stop
     result
   end
 
