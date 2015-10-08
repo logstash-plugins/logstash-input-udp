1f8e649717dc9b4e437fc57d29fb7fa6ed31b810
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-10-08 18:12:18 +0200

    skip the need to use another rspec.configure call as this breaks the way we do integration test in LS core for plugins, this is not standard rspec, but is a necessary workaround for the way we do test in LS core for all plugins
    Fixes #15

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,7 @@ class LogStash::Inputs::Udp
   attr_reader :udp
 end
 
-module UdpHelpers
+class UdpHelpers
 
   def input(plugin, size, &block)
     queue = Queue.new
@@ -20,15 +20,11 @@ module UdpHelpers
     sleep 0.1 until (plugin.udp && !plugin.udp.closed?)
     block.call
     sleep 0.1 while queue.size != size
-    result = nevents.times.inject([]) do |acc|
+    result = size.times.inject([]) do |acc|
       acc << queue.pop
     end
     plugin.do_stop
     result
   end
 
-end
-
-RSpec.configure do |c|
-  c.include UdpHelpers
 end
