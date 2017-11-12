e1889e92f8b92741f15f9b60d97348899b613ec8
Jordan Sissel <jls@semicomplete.com>
2017-11-12 08:52:53 -0800

    Fix IPv6 support.
    In Logstash 5.x and below, JRuby 1.7.x is used, and
    UDPSocket.new(Socket::AF_INET) somehow accidentally works with IPv6.
    
    Logstash 6.x uses JRuby 9.x where this now behaves correctly (AF_INET is
    ipv4 only), but triggers a bug where ipv6 was accidentally disabled in
    the udp input. Oops!
    
    This change is small and adds tests to cover the
    default scenario that 127.0.0.1 and ::1 both work for the default.

--- spec/support/client.rb
+++ spec/support/client.rb
@@ -1,21 +1,25 @@
 # encoding: utf-8
 require "socket"
+require "ipaddr"
 
 module LogStash::Inputs::Test
 
   class UDPClient
     attr_reader :host, :port, :socket
 
-    def initialize(port)
+    def initialize(port, host = "0.0.0.0")
+      @host = host
       @port = port
-      @host = "0.0.0.0"
-      @socket = UDPSocket.new
-      socket.connect(host, port)
+      if IPAddr.new(@host).ipv6?
+        @socket = UDPSocket.new(Socket::AF_INET6)
+      elsif IPAddr.new(@host).ipv4?
+        @socket = UDPSocket.new(Socket::AF_INET)
+      end
+      @socket.connect(host, port)
     end
 
     def send(msg)
       begin
-        socket.connect(host, port) if socket.closed?
         socket.send(msg, 0)
       rescue  => e
         puts("send exception, retrying", e.inspect)
